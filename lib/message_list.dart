d7bfda7e2c3075bf3895b0ef94c91ee42e598808
Mamun <bizzmamun@gmail.com>
2020-06-06 00:07:00 +0600

    don't know it'll show or not let's try

--- lib/message_list.dart
+++ lib/message_list.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 
-import 'package:email_app/messages.dart';
+import 'package:email_app/message.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class MessageList extends StatefulWidget {
   final String title;
-  MessageList({this.title});
+  const MessageList({Key key, this.title}) : super(key: key);
 
   @override
-  _MessageListState createState() => _MessageListState();
+  State<StatefulWidget> createState() => _MessageListState();
 }
 
 class _MessageListState extends State<MessageList> {
@@ -17,45 +17,47 @@ class _MessageListState extends State<MessageList> {
   Future loadMessageList() async {
     // here we are using async await because of late latency and it's huge for computer.
     // we are using load string from rootBundle object for making that json to string.
-    String content = await rootBundle.loadString('data/messages.json');
+    String content = await rootBundle.loadString('data/message.json');
     // and we are decoding that string data to List data by performing json.decode method.
-    List<Messages> collection = json.decode(content);
+    List collection = json.decode(content);
+    List<Message> _messages =
+        collection.map((json) => Message.fromJson(json)).toList();
+    // [1,2,3,4].map((el) => el+1) --> [2,3,4,5]
     // setState is used for saying flutter that this state should exist. Like the enter button to say
     // the computer that we want certain action to perform.
     setState(() {
       // here we are assining the json data from collection to the empty list of messages so that it can
       // show the messages to the screen , and its showing in the below ListTile widget.
       // we are making a separate json data file and accessing it with decoding it.
-      messages = collection;
+      messages = _messages;
     });
   }
 
-  @override
   void initState() {
     loadMessageList();
     super.initState();
   }
 
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(this.widget.title),
+        title: Text(widget.title),
       ),
       body: ListView.separated(
         itemCount: messages.length,
         separatorBuilder: (context, index) => Divider(),
         itemBuilder: (BuildContext context, int index) {
-          Messages message = messages[index];
+          Message message = messages[index];
           return ListTile(
             title: Text(message.subject),
+            isThreeLine: true,
+            leading: CircleAvatar(
+              child: Text('M'),
+            ),
             subtitle: Text(
               message.body,
               maxLines: 2,
               overflow: TextOverflow.ellipsis,
-            ),
-            leading: CircleAvatar(
-              child: Text('M'),
             ),
           );
         },
@@ -63,3 +65,4 @@ class _MessageListState extends State<MessageList> {
     );
   }
 }
+// _MessageListState
--- lib/messages.dart
+++
@@ -1,6 +0,0 @@
-class Messages {
-  final subject;
-  final body;
-
-  Messages({this.subject, this.body});
-}
